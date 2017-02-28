2bfebd2127ba5ac27c6a3375078b0a51df59973c
shawn <851294438@qq.com>
2017-03-01 00:49:26 +0800

    第二天登录界面&登录校验

--- js/common/main.js
+++ js/common/main.js
@@ -12,15 +12,14 @@ requirejs.config({
         bootstrap: '/lib/bootstrap/js/bootstrap.min',
         userList:'/js/user/list',
         userProfile:'/js/user/profile',
-        homeLogin:'/js/home/login',
         common:'/js/common/common',
-        login:'/js/common/login',
+        login:'/js/home/login',
         jqueryCookie:'/lib/jquery-cookie/jquery.cookie'
     },
     //自己写的路径配置
 
     shim:{
-        boostrap:{
+        bootstrap:{
             deps:['jquery']
 
             }
@@ -40,25 +39,39 @@ require(['jquery','bootstrap','common']);
     /*
     *  1 登录页
     *  1.1 没有SESSID 不用管
-    *  1.2 有 SESSID,跳转到登录页
+    *  1.2 有 SESSID,跳转到首页
     *
     *  2 其它页
     *  2.1 没有SESSID 跳转到登录页
     *  2.2 有SESSID 不用管
     * */
-    switch(pathname){
-        case'/html/user/list.html':
-        require(['userList']);
-        break;
-        case'/html/user/profile.html':
-        require(['userProfile']);
-        break;
-        case'/html/home/login.html':
-        require(['homeLogin']);
-        break;
-        case'/html/course/add.html':
-        require(['courseAdd']);
-        break;
+    //注意这里的jqueryCookie内置了define,但是没有返回值,所以可以依赖于jquery或者配置exports一个返回值
+    require(['jquery','jqueryCookie'],function($,undefined){
+        var seeID  = $.cookie('PHPSESSID');
+        if(pathname==='/html/home/login.html'&&seeID){
+            location.href='/'
+        }else if(pathname!=='/html/home/login.html'&&!seeID){
+            location.href='/html/home/login.html';
 
-    }
+        }
+            //因为function是一个回调函数,异步执行,等待前面的模块加载完才执行
+            //如果没有发生页面跳转,就加载对应的js
+            switch(pathname){
+                case'/html/user/list.html':
+                    require(['userList']);
+                    break;
+                case'/html/user/profile.html':
+                    require(['userProfile']);
+                    break;
+                case'/html/home/login.html':
+                    require(['login']);
+                    break;
+                case'/html/course/add.html':
+                    require(['courseAdd']);
+                    break;
+
+            }
+
+    })
+
 })(window)
--- js/home/login.js
+++ js/home/login.js
@@ -3,14 +3,23 @@
  */
 define(['jquery'],function($){
 
-    //监听form-login
+   /*
+   *  1 先监听form表单的提交事件
+   *  2 事件回调中return false 阻止默认的提交
+   *  3 事件回调中通过ajax的方式 发送表单数据
+   *  4 如果返回结果中code为200 证明登录成功 跳转到首页   * */
     $('#form-login').on('submit',function(){
         $.ajax({
+            //请求数据的地址称为接口,
+            //前端获取后端的数据 通过url地址
             url:'/v6/login',
             type:'post',
             data:$(this).serialize(),
             success:function(data){
-                location.herf='/'
+                if(data.code===200){
+                    location.href='/';
+                }
+
             }
         })
         return false;
